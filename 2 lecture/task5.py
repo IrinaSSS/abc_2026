092d50497c3b2c84a5b284649afe8bcf735d26fb
IrinaSSS <104759887+IrinaSSS@users.noreply.github.com>
2026-09-16 16:20:42 +0300

    добавила файлы по второй лекции

---
+++ 2 lecture/task5.py
@@ -0,0 +1,22 @@
+#todo: Написать программу, которая считывает два числа и выводит их сумму, разность, частное, произведение,
+# результат целочисленного деления, результат деления с остатком, результат возведения в степень.
+
+num1 = float(input("введите число: "))
+num2 = float(input("введите число: "))
+
+print("num1 + num2 = ", num1 + num2)
+print("num1 - num2 = ", num1 - num2)
+print("num1 / num2 = ", num1 / num2)
+print("num1 * num2 = ", num1 * num2)
+print("num1 // num2 = ", num1 // num2)
+print("num1 % num2 = ", num1 % num2)
+print("num1^(num2) = ", pow(num1,num2))
+
+
+
+
+
+
+
+
+
